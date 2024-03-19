29684703fdd1ee9cf2f9c66a7ed9f8ffc690f4cc
yevpy <eugene2piskun@gmail.com>
2024-03-19 02:04:55 +0200

    feat: Add Task manager models and migrations
    - Added TaskType model to represent types of tasks
    - Added Position model to represent worker positions
    - Extended AbstractUser to create Worker model with position field
    - Implemented Task model for task manager
    - Defined relationships between Task, TaskType, Worker, and Position models
    - Generated and applied migrations for the new models

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_manager.Worker"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, default=None, null=True)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("URG", "Urgent"),
+        ("HIGH", "High priority"),
+        ("LOW", "Low priority"),
+    ]
+
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=255)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES, default="URG")
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="workers")
+
+    def __str__(self):
+        return self.name
